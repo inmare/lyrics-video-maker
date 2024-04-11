622d2bfde640e85dbad7a1ddcb34e441f0f6424d
inmare <yatsumehole@naver.com>
2024-04-12 03:58:01 +0900

    Change subtitle selecting logic and add subtitle moving logic

--- script.js
+++ script.js
@@ -1,118 +1,80 @@
-// import SubtitleDiv from "./SubtitleDiv";
+import SubtitleDiv from "./SubtitleDiv";
+import SubtitleArray from "./SubtitleArray";
+import { SUBTITLE_FORMAT } from "./globals";
 
-let subtitleArray = [];
+const SUBTITLE_ARRAY = new SubtitleArray();
 
 const addSubtitleBtn = document.querySelector("#add-subtitle");
 addSubtitleBtn.addEventListener("click", addSubtitleDiv);
 
 const subtitleContainer = document.querySelector("#subtitle-container");
 
-function addSubtitleDiv() {
-  const subtitleInfo = {
-    length: 100,
-    time: 0,
-    context: "test",
-  };
+let shiftX = null;
+let isMouseDown = false;
+let isMouseMoving = false;
 
-  const subtitleDiv = new SubtitleDiv(subtitleInfo);
-  if (subtitleArray.length > 0) {
-    const lastSubtitle = subtitleArray[subtitleArray.length - 1];
+document.addEventListener("mousedown", (e) => {
+  if (e.target.classList.contains("subtitle")) {
+    if (SUBTITLE_ARRAY.selected === null) {
+      SUBTITLE_ARRAY.selected = SUBTITLE_ARRAY.find(
+        (subtitle) => subtitle.getDiv() === e.target,
+      );
+      SUBTITLE_ARRAY.selected.highlightToggle();
+    } else if (SUBTITLE_ARRAY.selected instanceof SubtitleDiv) {
+      SUBTITLE_ARRAY.selected.highlightToggle();
+      SUBTITLE_ARRAY.selected = SUBTITLE_ARRAY.find(
+        (subtitle) => subtitle.getDiv() === e.target,
+      );
+      SUBTITLE_ARRAY.selected.highlightToggle();
+    }
+    shiftX =
+      e.clientX - SUBTITLE_ARRAY.selected.getDiv().getBoundingClientRect().left;
+    isMouseDown = true;
+  } else {
+    if (SUBTITLE_ARRAY.selected instanceof SubtitleDiv) {
+      SUBTITLE_ARRAY.selected.highlightToggle();
+      SUBTITLE_ARRAY.selected = null;
+    }
+  }
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (SUBTITLE_ARRAY.selected instanceof SubtitleDiv) {
+    if (isMouseDown) {
+      const time = e.clientX - shiftX;
+      SUBTITLE_ARRAY.selected.setTime(time);
+      isMouseMoving = true;
+    }
+  }
+});
+
+document.addEventListener("mouseup", (e) => {
+  if (SUBTITLE_ARRAY.selected instanceof SubtitleDiv) {
+    if (isMouseMoving && isMouseDown) {
+      const offset = e.clientX - SUBTITLE_ARRAY.selected.getTime();
+      const time = e.clientX - offset;
+      SUBTITLE_ARRAY.selected.setTime(time);
+    }
+    isMouseDown = false;
+    isMouseMoving = false;
+  }
+});
+
+function addSubtitleDiv() {
+  const subtitleDiv = new SubtitleDiv(SUBTITLE_FORMAT);
+  subtitleDiv.setTime(0);
+  subtitleDiv.setLength(100);
+  subtitleDiv.setContext("Subtitle");
+
+  if (SUBTITLE_ARRAY.length > 0) {
+    const lastSubtitle = SUBTITLE_ARRAY[SUBTITLE_ARRAY.length - 1];
     subtitleDiv.setTime(lastSubtitle.getTime() + lastSubtitle.getLength());
   }
-  subtitleArray.push(subtitleDiv);
-  subtitleContainer.appendChild(subtitleDiv.div);
+
+  SUBTITLE_ARRAY.push(subtitleDiv);
+  subtitleContainer.appendChild(subtitleDiv._div);
 }
 
 window.onmousemove = () => {
   // if mousedown is ture and div is selected, then drag the div
 };
-
-class SubtitleDiv {
-  constructor({ length, time, context }) {
-    // set default values
-    this._length = length;
-    this._time = time;
-    this._context = context;
-    // creative div
-    this.div = document.createElement("div");
-    this.div.style.width = `${length}px`;
-    this.div.style.left = `${time}px`;
-    this.div.innerHTML = context;
-    this.div.classList.add("subtitle");
-    // this.div.onmousedown = () => {
-    //   this.isMousedown = true;
-    //   this.highlightDiv();
-    // };
-    // this.div.onmousemove = (event) => {
-    //   this.dragDiv(event);
-    // };
-    // this.div.onmouseup = () => {
-    //   this.isMousedown = false;
-    // };
-    this.div.onclick = () => {
-      this.highlightDiv();
-    };
-    // div related variable
-    this.isMousedown = false;
-  }
-
-  setLength(length) {
-    this._length = length;
-    this.div.style.width = `${length}px`;
-  }
-
-  setTime(time) {
-    this._time = time;
-    this.div.style.left = `${time}px`;
-  }
-
-  setContext(context) {
-    this._context = context;
-    this.div.innerHTML = context;
-  }
-
-  getLength() {
-    return this._length;
-  }
-
-  getTime() {
-    return this._time;
-  }
-
-  getContext() {
-    return this._context;
-  }
-
-  highlightDiv() {
-    for (let sub of subtitleArray) {
-      sub.div.classList.remove("highlight");
-    }
-    // if (this.isMousedown) {
-    //   this.div.classList.add("highlight");
-    // } else {
-    //   this.div.classList.remove("highlight");
-    // }
-    this.div.classList.toggle("highlight");
-  }
-
-  dragDiv(event) {
-    // console.log(this.isMousedown);
-    if (this.isMousedown) {
-      const mousePos = event.clientX;
-      const divPos = this.div.getBoundingClientRect().left;
-      const diff = mousePos - divPos;
-      this.div.style.left = `${mousePos - diff}px`;
-    }
-  }
-}
-
-class SubtitleArray {
-  constructor() {
-    this._subtitleArray = [];
-    this._selected = [];
-  }
-
-  static push(subtitle) {
-    this._subtitleArray.push(subtitle);
-  }
-}
