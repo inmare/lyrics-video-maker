637b50c256c044d1c66701d5a9fe8e65b660ce0d
inmare <yatsumehole@naver.com>
2024-04-15 18:15:30 +0900

    Add temporary timelinelength property

--- LyricsArray.js
+++ LyricsArray.js
@@ -1,6 +1,7 @@
 export default class LyricsArray extends Array {
-  constructor() {
+  constructor(timelineLength) {
     super();
     this.selected = null;
+    this.timelineLength = timelineLength;
   }
 }
