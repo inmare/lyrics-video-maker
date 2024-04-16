3c11b36ca01f17560a3bde5f7bb649386b59c5db
inmare <yatsumehole@naver.com>
2024-04-16 14:16:00 +0900

    Add time2str function

--- utils.js
+++ utils.js
@@ -8,4 +8,16 @@ function px2time(px) {
   return Math.round((px / SECOND_LEN_PX) * 1000) / 1000;
 }
 
-export { time2px, px2time };
+function time2str(secNumber) {
+  const min = Math.floor(secNumber / 60);
+  const sec = Math.floor(secNumber) % 60;
+  const msec = Math.floor((secNumber % 1) * 1000);
+
+  const minStr = min.toString().padStart(2, "0");
+  const secStr = sec.toString().padStart(2, "0");
+  const msecStr = msec.toString().padStart(3, "0");
+
+  return [minStr, secStr, msecStr];
+}
+
+export { time2px, px2time, time2str };
