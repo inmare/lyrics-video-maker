d15a024bd623aa10444aeed9f6f517e207ccf491
inmare <yatsumehole@naver.com>
2024-04-16 11:59:59 +0900

    Change LyricsArray class to not extend Array class

--- LyricsArray.js
+++ LyricsArray.js
@@ -1,7 +1,26 @@
-export default class LyricsArray extends Array {
-  constructor(timelineLength) {
-    super();
-    this.selected = null;
-    this.timelineLength = timelineLength;
+export default class LyricsArray {
+  static _lyricsArr = [];
+  static selected = null;
+
+  constructor() {}
+
+  static push(lyrics) {
+    this._lyricsArr.push(lyrics);
+  }
+
+  static find(callback) {
+    return this._lyricsArr.find(callback);
+  }
+
+  static indexOf(lyrics) {
+    return this._lyricsArr.indexOf(lyrics);
+  }
+
+  static getLyrics(idx) {
+    return this._lyricsArr[idx];
+  }
+
+  static getLength() {
+    return this._lyricsArr.length;
   }
 }
